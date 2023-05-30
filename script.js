315ad5be7437972b73b334f7dd8a2ff558988b8e
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-05-31 01:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function hii(){
+	document.getElementById('status').innerHTML=""
+	const x=document.createElement('h1');
+	
+	x.innerHTML="Entered Metaverse";
+	document.getElementById('status').appendChild(x)};
